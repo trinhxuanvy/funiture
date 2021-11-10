4d3bdd6874134a93e810b9f977e9c8dbbc6e3688
minh-tri <cmtri2601@gmail.com>
2021-11-10 15:31:54 +0700

    tri single-product

--- routes/shop.route.js
+++ routes/shop.route.js
@@ -6,6 +6,10 @@ router.get("/categories", (req, res, next) => {
     res.render("categories");
 });
 
+router.get("/single-product", (req, res, next) => {
+    res.render("single-product");
+});
+
 router.get("/admin/profile", (req, res, next) => {
     res.render("admin/profile", { pageName: " profile " });
 });
