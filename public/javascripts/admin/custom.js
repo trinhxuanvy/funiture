49ef0fe64ec0c484d355edc27f5853bd5b23bab5
Mr.Trinh <trinhxuanvy1@gmail.com>
2021-11-15 17:56:40 +0700

    create fully admin page

--- public/javascripts/admin/custom.js
+++ public/javascripts/admin/custom.js
@@ -60,4 +60,45 @@ $(document).ready(function() {
     //         theme: "bootstrap-5",
     //     });
     // });
+
+    // Xử lý popup single product admin page
+    $(function() {
+        $("#product-single-close").click(function(e) {
+            e.preventDefault();
+
+            $("#product_image_area").css("display", "none");
+        });
+    });
+
+    $(function() {
+        const product_overview = $('#vertical');
+        if (product_overview.length) {
+            product_overview.lightSlider({
+                gallery: true,
+                item: 1,
+                vertical: true,
+                verticalHeight: 420,
+                thumbItem: 4,
+                slideMargin: 0,
+                speed: 600,
+                autoplay: true,
+                responsive: [{
+                        breakpoint: 991,
+                        settings: {
+                            item: 1,
+
+                        }
+                    },
+                    {
+                        breakpoint: 576,
+                        settings: {
+                            item: 1,
+                            slideMove: 1,
+                            verticalHeight: 350,
+                        }
+                    }
+                ]
+            });
+        }
+    });
 });
--- routes/shop.route.js
+++ routes/shop.route.js
@@ -14,4 +14,16 @@ router.get("/admin/profile", (req, res, next) => {
     res.render("admin/profile", { pageName: " profile " });
 });
 
+router.get("/admin/users", (req, res, next) => {
+    res.render("admin/users", { pageName: " users " });
+});
+
+router.get("/admin/products", (req, res, next) => {
+    res.render("admin/products", { pageName: " products " });
+});
+
+router.get("/admin/category", (req, res, next) => {
+    res.render("admin/category", { pageName: " category " });
+});
+
 module.exports = router;
