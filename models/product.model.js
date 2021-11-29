47c758e859dce6fd939b19cd52e9410f635be05d
Mr.Trinh <trinhxuanvy1@gmail.com>
2021-11-29 22:27:38 +0700

    update admin

--- models/product.model.js
+++ models/product.model.js
@@ -1,89 +1,94 @@
 const mongooes = require("mongoose");
 const Schema = mongooes.Schema;
 
-const Product = new Schema({
+const Product = new Schema(
+  {
     prodName: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
     },
     description: {
-        type: String,
-        required: false,
+      type: String,
+      required: false,
     },
     amount: {
-        type: Number,
-        default: 0,
-        min: 0
+      type: Number,
+      default: 0,
+      min: 0,
     },
     price: {
-        type: Number,
-        required: true,
+      type: Number,
+      required: true,
     },
     status: {
-        type: Boolean,
-        default: true
+      type: Boolean,
+      default: true,
     },
-    prodImage: [{
+    prodImage: [
+      {
         imageLink: {
-            type: String,
-            required: false
+          type: String,
+          required: false,
         },
         type: {
-            type: String,
-            required: true
-        }
-    }],
+          type: String,
+          required: true,
+        },
+      },
+    ],
     prodTypeId: {
-        type: Schema.Types.ObjectId,
-        ref: "Category",
-        required: true
+      type: Schema.Types.ObjectId,
+      ref: "Category",
+      required: true,
     },
     prodTypeName: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     brandId: {
-        type: Schema.Types.ObjectId,
-        ref: "Brand",
-        required: true
+      type: Schema.Types.ObjectId,
+      ref: "Brand",
+      required: true,
     },
     brandName: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     prodName: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     color: {
-        type: String,
-        required: false
+      type: String,
+      required: false,
     },
     width: {
-        type: Number,
-        required: false
+      type: Number,
+      required: false,
     },
     height: {
-        type: Number,
-        required: false
+      type: Number,
+      required: false,
     },
     depth: {
-        type: Number,
-        required: false
+      type: Number,
+      required: false,
     },
     weight: {
-        type: Number,
-        required: false
+      type: Number,
+      required: false,
     },
     soldQuantity: {
-        type: Number,
-        default: 0,
-        min: 0
-    }
-}, {
+      type: Number,
+      default: 0,
+      min: 0,
+    },
+  },
+  {
     timestamps: true,
     versionKey: false,
     collection: "Product",
-});
+  }
+);
 
 module.exports = mongooes.model("Product", Product);
--- public/javascripts/admin/custom.js
+++ public/javascripts/admin/custom.js
@@ -1,210 +1,246 @@
 $(document).ready(function () {
-    $(function () {
-        $('#datetimepicker1').datepicker();
+  $(function () {
+    $("#datetimepicker1").datepicker();
+  });
+
+  // Xử lý toggle menu
+  $(function () {
+    $("#menu-toggle").click(function (e) {
+      e.preventDefault();
+      const sideBar = $("#sidebar");
+      const main = $("#main");
+      const hasClass = $(sideBar).hasClass("show");
+
+      if (hasClass) {
+        $(sideBar).removeClass("show");
+        $(main).removeClass("move");
+      } else {
+        $(sideBar).addClass("show");
+        $(main).addClass("move");
+      }
     });
+  });
 
-    // Xử lý toggle menu
-    $(function () {
-        $("#menu-toggle").click(function (e) {
-            e.preventDefault();
-            const sideBar = $("#sidebar");
-            const main = $("#main");
-            const hasClass = $(sideBar).hasClass("show");
-
-            if (hasClass) {
-                $(sideBar).removeClass("show");
-                $(main).removeClass("move");
-            } else {
-                $(sideBar).addClass("show");
-                $(main).addClass("move");
-            }
-        });
+  // Xử lý validate form
+  $(function () {
+    $("#addUserForm").validate({
+      rules: {
+        email: {
+          required: true,
+          email: true,
+        },
+        username: {
+          required: true,
+          minlength: 8,
+          maxlength: 56,
+        },
+        phone: {
+          required: true,
+        },
+      },
+      messages: {
+        email: {
+          required: "Please enter your email.",
+          email: "Please check your email.",
+        },
+        username: {
+          required: "Please enter your name",
+          minlength: "Your username must be at least 8 characters long.",
+          maxlength: "Your username must be at most 52 characters long.",
+        },
+        phone: {
+          required: "Please enter your phone number.",
+        },
+      },
     });
+  });
 
-    // Xử lý validate form
-    $(function () {
-        $("#addUserForm").validate({
-            rules: {
-                email: {
-                    required: true,
-                    email: true
-                },
-                username: {
-                    required: true,
-                    minlength: 8,
-                    maxlength: 56
-                },
-                phone: {
-                    required: true,
-                }
+  // $(function() {
+  //     $("select").select2({
+  //         theme: "bootstrap-5",
+  //     });
+  // });
+
+  // Xử lý popup single product admin page
+  $(function () {
+    $("#product-single-close").click(function (e) {
+      e.preventDefault();
+
+      $("#product_image_area").css("display", "none");
+    });
+  });
+
+  $(function () {
+    const product_overview = $("#vertical");
+    if (product_overview.length) {
+      product_overview.lightSlider({
+        gallery: true,
+        item: 1,
+        vertical: true,
+        verticalHeight: 420,
+        thumbItem: 4,
+        slideMargin: 0,
+        speed: 600,
+        autoplay: true,
+        responsive: [
+          {
+            breakpoint: 991,
+            settings: {
+              item: 1,
             },
-            messages: {
-                email: {
-                    required: "Please enter your email.",
-                    email: "Please check your email."
-                },
-                username: {
-                    required: "Please enter your name",
-                    minlength: "Your username must be at least 8 characters long.",
-                    maxlength: "Your username must be at most 52 characters long.",
-                },
-                phone: {
-                    required: "Please enter your phone number."
-                }
+          },
+          {
+            breakpoint: 576,
+            settings: {
+              item: 1,
+              slideMove: 1,
+              verticalHeight: 350,
+            },
+          },
+        ],
+      });
+    }
+  });
+
+  // Xử lý chức năng xóa sản phẩm
+  $(function () {
+    const btnDeleteProd = $(".btn-delete-prod");
+    const trProd = $(".tr-prod");
+
+    for (let i = 0; i < btnDeleteProd.length; i++) {
+      $(btnDeleteProd[i]).click(function (e) {
+        e.preventDefault();
+
+        const url = new URL(window.location.href);
+
+        $.ajax({
+          method: "get",
+          contentType: "application/json",
+          url:
+            url.origin +
+            url.pathname +
+            "/delete/" +
+            $(btnDeleteProd[i]).val() +
+            url.search,
+          dataType: "json",
+          success: function (response) {
+            if (response.success) {
+              if (!response.status) {
+                $(trProd[i]).css("opacity", "0.5");
+              } else {
+                $(trProd[i]).css("opacity", "1");
+              }
             }
+          },
         });
-    });
+      });
+    }
+  });
 
-    // $(function() {
-    //     $("select").select2({
-    //         theme: "bootstrap-5",
-    //     });
-    // });
+  $(function () {
+    const input = $("#table-product input");
+    const select = $("#table-product select");
 
-    // Xử lý popup single product admin page
-    $(function () {
-        $("#product-single-close").click(function (e) {
-            e.preventDefault();
+    for (let i = 0; i < select.length; i++) {
+      $(select[i]).change(function (e) {
+        e.preventDefault();
 
-            $("#product_image_area").css("display", "none");
+        const id = $(select[i].parentNode.parentNode).attr("id");
+        const url = new URL(window.location.href);
+        const name = $(select[i]).attr("name");
+        var postData = {};
+        postData[name] = $(select[i]).val();
+        console.log(postData);
+
+        $.ajax({
+          method: "post",
+          contentType: "application/json",
+          url: url.origin + url.pathname + "/update/" + id,
+          data: JSON.stringify(postData),
+          dataType: "json",
+          success: function (response) {
+            console.log(response);
+            showBoxModal();
+          },
         });
-    });
+      });
+    }
 
-    $(function () {
-        const product_overview = $('#vertical');
-        if (product_overview.length) {
-            product_overview.lightSlider({
-                gallery: true,
-                item: 1,
-                vertical: true,
-                verticalHeight: 420,
-                thumbItem: 4,
-                slideMargin: 0,
-                speed: 600,
-                autoplay: true,
-                responsive: [{
-                    breakpoint: 991,
-                    settings: {
-                        item: 1,
+    for (let i = 0; i < input.length; i++) {
+      if ($(input[i]).attr("type") != "file") {
+        $(input[i]).focusout(function (e) {
+          e.preventDefault();
 
-                    }
-                },
-                {
-                    breakpoint: 576,
-                    settings: {
-                        item: 1,
-                        slideMove: 1,
-                        verticalHeight: 350,
-                    }
-                }
-                ]
-            });
-        }
-    });
+          const id = $(input[i].parentNode.parentNode.parentNode).attr("id");
+          const url = new URL(window.location.href);
+          const name = $(input[i]).attr("name");
+          var postData = {};
+          postData[name] = $(input[i]).val();
 
-    // Xử lý chức năng xóa sản phẩm
-    $(function () {
-        const btnDeleteProd = $(".btn-delete-prod");
-        const trProd = $(".tr-prod");
-        for (let i = 0; i < btnDeleteProd.length; i++) {
-            $(btnDeleteProd[i]).click(function (e) {
-                e.preventDefault();
+          $.ajax({
+            method: "post",
+            contentType: "application/json",
+            url: url.origin + url.pathname + "/update/" + id,
+            data: JSON.stringify(postData),
+            dataType: "json",
+            success: function (response) {
+              console.log(response);
+              showBoxModal();
+            },
+          });
+        });
+      } else {
+        $(input[i]).change(function (e) {
+          e.preventDefault();
+
+          const id = $(input[i].parentNode.parentNode.parentNode).attr("id");
+          const url = new URL(window.location.href);
+          const name = $(input[i]).attr("name");
+          var formData = new FormData();
+          formData.append(name, $(input[i])[0].files[0]);
+
+          $.ajax({
+            method: "post",
+            processData: false,
+            contentType: false,
+            url: url.origin + "/admin/upload",
+            data: formData,
+            enctype: "multipart/form-data",
+            success: function (response) {
+              if (response?.success) {
+                const link = response?.url;
+                var postData = {};
+                postData[name] = response?.url;
+
                 $.ajax({
-                    method: "get",
-                    contentType: "application/json",
-                    url: window.location.href + "/delete/" + $(btnDeleteProd[i]).val(),
-                    dataType: "json",
-                    success: function (response) {
-                        if (response.success) {
-                            if (response.status) {
-                                $(trProd[i]).css("opacity", "0.5");
-                            } else {
-                                $(trProd[i]).css("opacity", "1");
-                            }
-                        }
-                    }
+                  method: "post",
+                  contentType: "application/json",
+                  url: url.origin + url.pathname + "/update/" + id,
+                  data: JSON.stringify(postData),
+                  dataType: "json",
+                  success: function (response) {
+                    input[i].parentNode.childNodes[1].src = link;
+                    console.log(response);
+                    showBoxModal();
+                  },
                 });
-            });
-        }
-
-    });
-
-    // Xử lý hiển thị khi thay đổi image
-    $(function () {
-        const btnPrimaryImage = $("#wrapperContent .primary-image-input");
-        const image = $("#wrapperContent .primary-image img");
-
-        for (let i = 0; i < btnPrimaryImage.length; i++) {
-            $(btnPrimaryImage[i]).change(function (e) {
-                e.preventDefault();
-
-                const file = btnPrimaryImage[i].files;
-                const fileReader = new FileReader();
-
-                fileReader.onload = function (e) {
-                    const src = e.target.result;
-                    image[i].src = src;
-                }
-
-                fileReader.readAsDataURL(file[0]);
-            });
-        }
-
-        const btnSecondaryImage1 = $("#wrapperContent .secondary-image-input-1");
-        const imageSecondary1 = $("#wrapperContent .secondary-image-1 img");
-
-        for (let i = 0; i < btnSecondaryImage1.length; i++) {
-            $(btnSecondaryImage1[i]).change(function (e) {
-                e.preventDefault();
-
-                const file = btnSecondaryImage1[i].files;
-                const fileReader = new FileReader();
-
-                fileReader.onload = function (e) {
-                    const src = e.target.result;
-                    imageSecondary1[i].src = src;
-                }
-
-                fileReader.readAsDataURL(file[0]);
-            });
-        }
-
-        const btnSecondaryImage2 = $("#wrapperContent .secondary-image-input-2");
-        const imageSecondary2 = $("#wrapperContent .secondary-image-2 img");
-
-        for (let i = 0; i < btnSecondaryImage2.length; i++) {
-            $(btnSecondaryImage2[i]).change(function (e) {
-                e.preventDefault();
-
-                const file = btnSecondaryImage2[i].files;
-                const fileReader = new FileReader();
-
-                fileReader.onload = function (e) {
-                    const src = e.target.result;
-                    imageSecondary2[i].src = src;
-                }
-
-                fileReader.readAsDataURL(file[0]);
-            });
-        }
-
-        const btnSecondaryImage3 = $("#wrapperContent .secondary-image-input-3");
-        const imageSecondary3 = $("#wrapperContent .secondary-image-3 img");
-
-        for (let i = 0; i < btnSecondaryImage3.length; i++) {
-            $(btnSecondaryImage3[i]).change(function (e) {
-                e.preventDefault();
-
-                const file = btnSecondaryImage3[i].files;
-                const fileReader = new FileReader();
-
-                fileReader.onload = function (e) {
-                    const src = e.target.result;
-                    imageSecondary3[i].src = src;
-                }
-
-                fileReader.readAsDataURL(file[0]);
-            });
-        }
-    });
+              }
+            },
+          });
+        });
+      }
+    }
+  });
 });
+
+function showBoxModal() {
+  const modal = $("#boxModal");
+  $(modal[0]).css("right", "-100%");
+  setTimeout(() => {
+    $(modal[0]).css("right", "32px");
+  }, 400);
+
+  setTimeout(() => {
+    $(modal[0]).css("right", "-100%");
+  }, 2000);
+}
